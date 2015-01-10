85160c8fe218a3cdd8ae6159a702404b3a1f66ad
GHC124 <chung.pv0795@gmail.com>
2015-01-10 16:07:33 +0700

    Create DialogFragment example

--- app/src/main/java/android/ghc/com/trainningexample/DialogFragmentExample.java
+++ app/src/main/java/android/ghc/com/trainningexample/DialogFragmentExample.java
@@ -29,27 +29,6 @@ public class DialogFragmentExample extends DialogFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mNum = getArguments().getInt("num");
-
-        // Pick a style based on the num.
-        int style = DialogFragment.STYLE_NORMAL, theme = 0;
-        switch ((mNum-1)%6) {
-            case 1: style = DialogFragment.STYLE_NO_TITLE; break;
-            case 2: style = DialogFragment.STYLE_NO_FRAME; break;
-            case 3: style = DialogFragment.STYLE_NO_INPUT; break;
-            case 4: style = DialogFragment.STYLE_NORMAL; break;
-            case 5: style = DialogFragment.STYLE_NORMAL; break;
-            case 6: style = DialogFragment.STYLE_NO_TITLE; break;
-            case 7: style = DialogFragment.STYLE_NO_FRAME; break;
-            case 8: style = DialogFragment.STYLE_NORMAL; break;
-        }
-        switch ((mNum-1)%6) {
-            case 4: theme = android.R.style.Theme_Holo; break;
-            case 5: theme = android.R.style.Theme_Holo_Light_Dialog; break;
-            case 6: theme = android.R.style.Theme_Holo_Light; break;
-            case 7: theme = android.R.style.Theme_Holo_Light_Panel; break;
-            case 8: theme = android.R.style.Theme_Holo_Light; break;
-        }
-        setStyle(style, theme);
     }
 
     @Override
@@ -57,8 +36,7 @@ public class DialogFragmentExample extends DialogFragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_dialog, container, false);
         View tv = v.findViewById(R.id.text);
-        ((TextView)tv).setText("Dialog #" + mNum + ": using style "
-                + getNameForNum(mNum));
+        ((TextView)tv).setText("Dialog #" + mNum);
 
         // Watch for button clicks.
         Button button = (Button)v.findViewById(R.id.btnShowDialog);
@@ -69,12 +47,8 @@ public class DialogFragmentExample extends DialogFragment {
             }
         });
 
+       getDialog().setTitle("Dialog #" + mNum);
+
         return v;
-    }
-
-    private String getNameForNum(int num){
-        String name = "";
-
-        return name;
     }
 }
--- app/src/main/java/android/ghc/com/trainningexample/MainActivity.java
+++ app/src/main/java/android/ghc/com/trainningexample/MainActivity.java
@@ -1,17 +1,26 @@
 package android.ghc.com.trainningexample;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    private int mStackLevel = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnShowDialog = (Button)findViewById(R.id.btnShowDialog);
+        btnShowDialog.setOnClickListener(this);
     }
 
 
@@ -37,8 +46,31 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void showDialog(){
 
+
+    public void showDialog(){
+        mStackLevel++;
+        // DialogFragment.show() will take care of adding the fragment
+        // in a transaction.  We also want to remove any currently showing
+        // dialog, so make our own transaction and take care of that here.
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+
+        // Create and show the dialog.
+        DialogFragment newFragment = DialogFragmentExample.newInstance(mStackLevel);
+        newFragment.show(ft, "dialog");
     }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnShowDialog:
+                showDialog();
+                break;
+        }
+    }
 }
